3f5c7eaa6bdb7194882ca0e830c837d4f0b1fc31
Sebastian H. <maracuchoamericano@gmail.com>
2025-01-24 22:17:24 -0500

    working tools

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <p className='m-4 btn-primary btn'>hello world</p>
+    </>
+  )
+}
+
+export default App
